284c718ee1b0543c63be15fe7617ce56ea50ed28
kerneldroid <212222389+kerneldroid@users.noreply.github.com>
2026-08-29 16:07:39 +0000

    release: v1.2.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ extensions.configure<ApplicationExtension>("android") {
 
     defaultConfig {
         applicationId = packageName
-        this.versionCode = 20
-        this.versionName = "1.2.4"
+        this.versionCode = 21
+        this.versionName = "1.2.5"
         val updateCheckUrl =
             (project.findProperty("UPDATE_CHECK_URL") as? String)
                 ?: "https://raw.githubusercontent.com/kerneldroid/Dsu-Extended/master/other/updater.json"
